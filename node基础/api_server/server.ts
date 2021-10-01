41dbf765ab2974d4372c2498228a5466ba562461
maoxiaoxing <930649054@qq.com>
2021-10-01 21:25:35 +0800

    简单开启node服务

--- node基础/api_server/server.ts
+++ node基础/api_server/server.ts
@@ -1,1 +1,11 @@
-console.log(123)
+const express = require('express')
+
+const app = express()
+
+app.get('/', (req: any, res: any) => {
+  res.end('333')
+})
+
+app.listen(8080, () => {
+  console.log('服务开启')
+})
