65e1f3cc4a46ff5cffa318354a10c2cb037c3805
maoxiaoxing <930649054@qq.com>
2021-10-02 10:20:37 +0800

    process

--- node基础/05-process.js
+++ node基础/05-process.js
@@ -1,6 +1,36 @@
 // const fs = require('fs')
 
 // 资源：cpu 内存
-console.log(process.memoryUsage())
+// console.log(process.memoryUsage())
+// console.log(process.cpuUsage())
 
+// 运行目录
+// console.log(process.cwd())
+
+// // node版本
+// console.log(process.version)
+// console.log(process.versions)
+
+// // 系统是多少位的
+// console.log(process.arch)
+
+// 获取环境
+// console.log(process.env.NODE_ENV)
+
+// 系统变量
+// console.log(process.env.PATH)
+
+// 用户环境地址
+// console.log(process.env.USERPROFILE)
+
+// 系统平台
+// console.log(process.platform)
+
+// 获取参数
+// console.log(process.argv)
+
+console.log(process.pid)
+
+// 项目运行时间
+console.log(process.uptime())
 
