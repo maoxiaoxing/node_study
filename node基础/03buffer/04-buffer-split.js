7281c33c6fd32009625bf84c15b3162feef49c8a
maoxiaoxing <930649054@qq.com>
2021-10-03 16:10:39 +0800

    实现Buffer.split方法

---
+++ node基础/03buffer/04-buffer-split.js
@@ -0,0 +1,16 @@
+Buffer.prototype.split = function (sep) {
+  const len = Buffer.from(sep).length
+  const ret = []
+  let start = 0
+  let offset = 0
+
+  while((offset = this.indexOf(sep, start)) !== -1) {
+    ret.push(this.slice(start, offset))
+    start = offset + len
+  }
+  return ret
+}
+
+const buf = Buffer.from('123123123')
+const bufArr = buf.split('1').map((item) => item.toString())
+console.log(bufArr)
