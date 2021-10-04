19a6a8a8f30bd3de61138f0ce2d5e327133ba15f
maoxiaoxing <930649054@qq.com>
2021-10-04 09:55:20 +0800

    mkDir

--- node基础/04fs/08-mkdir-async.js
+++ node基础/04fs/08-mkdir-async.js
@@ -1,27 +1,50 @@
 const fs = require('fs')
 const path = require('path')
+const { promisify } = require('util')
 
-function mkDir(dirPath, cb) {
-  const parts = dirPath.split('/')
-  let index = 1
+// function mkDir(dirPath, cb) {
+//   const parts = dirPath.split('/')
+//   let index = 1
   
-  const next = () => {
-    if (index > parts.length) {
-      return cb && cb()
+//   const next = () => {
+//     if (index > parts.length) {
+//       return cb && cb()
+//     }
+//     const current = parts.slice(0, index++).join('/')
+//     fs.access(current, (err) => {
+//       if (err) {
+//         fs.mkdir(current, next)
+//       } else {
+//         next()
+//       }
+//     })
+//   } 
+//   next()
+// }
+
+// mkDir('a/b/c', () => {
+//   console.log('创建成功')
+// })
+
+
+const access = promisify(fs.access)
+const mkdir = promisify(fs.mkdir)
+
+async function myMkdir (dirPath, cb) {
+  const parts = dirPath.split('/')
+
+  for (let index = 1; index <= parts.length; index++) {
+    const current = parts.slice(0, index).join('/')
+    try {
+      await access(current)
+    } catch (err) {
+      await mkdir(current)
     }
-    const current = parts.slice(0, index++).join('/')
-    fs.access(current, (err) => {
-      if (err) {
-        fs.mkdir(current, next)
-      } else {
-        next()
-      }
-    })
-  } 
-  next()
+  }
+  cb && cb()
 }
 
-mkDir('a/b/c', () => {
+myMkdir('a/b/c', () => {
   console.log('创建成功')
 })
 
