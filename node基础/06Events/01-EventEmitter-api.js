0061bc4cdc349807226a3b3e8262931a404cdd8a
maoxiaoxing <930649054@qq.com>
2021-10-04 22:30:05 +0800

    once

--- node基础/06Events/01-EventEmitter-api.js
+++ node基础/06Events/01-EventEmitter-api.js
@@ -3,13 +3,28 @@ const EventEmitter = require('events')
 const ev = new EventEmitter()
 
 // on
-ev.on('event1', () => {
-  console.log('事件1')
-})
+// ev.on('event1', () => {
+//   console.log('事件1')
+// })
 
-ev.on('event1', () => {
-  console.log('事件2')
-})
+// ev.on('event1', () => {
+//   console.log('事件2')
+// })
 
-// emit
-ev.emit('event1')
+// // emit
+// ev.emit('event1')
+// ev.emit('event1')
+
+
+// once
+// ev.once('event', () =>{
+//   console.log('事件1')
+// })
+// ev.once('event', () =>{
+//   console.log('事件1---2')
+// })
+// ev.emit('event')
+// ev.emit('event')
+
+
+
