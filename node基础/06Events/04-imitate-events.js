ad938341e20adbdc0a229f21e211261e463485de
maoxiaoxing <930649054@qq.com>
2021-10-05 16:37:35 +0800

    on

--- node基础/06Events/04-imitate-events.js
+++ node基础/06Events/04-imitate-events.js
@@ -1,3 +1,11 @@
 function MyEvent () {
   this._event = Object.create(null)
 }
+
+MyEvent.prototype.on = function (type, cb) {
+  if (this._event[type]) {
+    this._event[type].push(cb)
+  } else {
+    this._event[type] = [cb]
+  }
+}
