b9527de3e89e908ca977c2b4c7da6b3680b443ce
maoxiaoxing <930649054@qq.com>
2021-10-06 22:23:44 +0800

    文件可读流事件与应用

--- node基础/07stream/06-fs-readstream.js
+++ node基础/07stream/06-fs-readstream.js
@@ -37,11 +37,14 @@ rs.on('close', () => {
   console.log('文件关闭了')
 })
 
+const bufferArr = []
 rs.on('data', (chunk) => {
-  console.log(chunk)
+  // console.log(chunk)
+  bufferArr.push(chunk)
 })
 
 rs.on('end', () => {
+  console.log(Buffer.concat(bufferArr).toString())
   console.log('数据清空之后')
 })
 
