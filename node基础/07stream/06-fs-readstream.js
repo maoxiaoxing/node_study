6f38803cf810401e68c56a778c7c928fbb828075
maoxiaoxing <930649054@qq.com>
2021-10-06 22:16:35 +0800

    readable

--- node基础/07stream/06-fs-readstream.js
+++ node基础/07stream/06-fs-readstream.js
@@ -11,12 +11,21 @@ const rs = fs.createReadStream('test.txt', {
   highWaterMark: 2
 })
 
-rs.on('data', (chunk) => {
-  console.log(chunk.toString())
-  rs.pause()
-  setTimeout(() => {
-    rs.resume()
-  }, 1000)
+// rs.on('data', (chunk) => {
+//   console.log(chunk.toString())
+//   rs.pause()
+//   setTimeout(() => {
+//     rs.resume()
+//   }, 1000)
+// })
+
+
+rs.on('readable', () => {
+  let data
+  while((data = rs.read(1)) !== null) {
+    console.log(data.toString())
+    console.log('----', rs._readableState.length)
+  }
 })
 
 
