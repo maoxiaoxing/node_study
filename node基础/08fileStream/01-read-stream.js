e0f873d411488389431d700189785ef1acbc3182
maoxiaoxing <930649054@qq.com>
2021-10-07 12:33:36 +0800

    模拟文件可读流

--- node基础/08fileStream/01-read-stream.js
+++ node基础/08fileStream/01-read-stream.js
@@ -12,17 +12,24 @@ class MyFileReadStream extends EventEmitter {
       highWaterMark,
     } = options
     this.path = path
+    this.mode = mode || 438
     this.flags = flags || 'r'
     this.autoClose = autoClose || true
     this.start = start || 0
     this.end = start
     this.highWaterMark = highWaterMark || 64 * 1024
+    this.readOffset = 0
 
     this.open()
+    this.on('newListener', (type) => {
+      console.log(type)
+      if (type === 'data') {
+        this.read()
+      }
+    })
   }
 
   open() {
-    console.log(121)
     // 原生 open 方法打开指定位置的文件
     fs.open(this.path, this.flags, this.mode, (err, fd) => {
       if (err) {
@@ -32,11 +39,45 @@ class MyFileReadStream extends EventEmitter {
       this.emit('open', fd)
     })
   }
+
+  read() {
+    if (typeof this.fd !== 'number') {
+      return this.once('open', this.read)
+    }
+    const buf = Buffer.alloc(this.highWaterMark)
+    fs.read(this.fd, buf, 0, this.highWaterMark, this.readOffset, (err, readBytes) => {
+      if (readBytes) {
+        this.readOffset += readBytes
+        this.emit('data', buf)
+        this.read()
+      } else {
+        this.emit('end')
+        this.close()
+      }
+    })
+  }
+
+  close() {
+    fs.close(this.fd, () => {
+      this.emit('close')
+    })
+  }
 }
 
 const rs = new MyFileReadStream('test.txt')
 
-rs.on('open', (fd) => {
-  console.log('open', fd)
-})
+// rs.on('open', (fd) => {
+//   console.log('open', fd)
+// })
 
+// rs.on('data', (chunk) => {
+//   console.log(chunk)
+// })
+
+// rs.on('end', () => {
+//   console.log('end')
+// })
+
+rs.on('close', () => {
+  console.log('close')
+})
