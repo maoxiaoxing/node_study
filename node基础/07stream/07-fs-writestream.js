cce3d17a7c5bad03502a50c4622b13b49740a760
maoxiaoxing <930649054@qq.com>
2021-10-07 10:33:02 +0800

    文件写入流

--- node基础/07stream/07-fs-writestream.js
+++ node基础/07stream/07-fs-writestream.js
@@ -9,11 +9,25 @@ const ws = fs.createWriteStream('text.txt', {
   highWaterMark: 3,
 })
 
-ws.write('小羊吃草', () => {
-  console.log('数据写入完成')
+// ws.write('小羊吃草', () => {
+//   console.log('数据写入完成')
+// })
+
+// // 写入字符串 或者 buffer
+// ws.write('123456', () => {
+//   console.log('数据写入完成')
+// })
+
+ws.on('open', (fd) => {
+  console.log('open', fd)
 })
 
-ws.write('123456', () => {
-  console.log('数据写入完成')
+ws.write('123')
+
+ws.on('close', () => {
+  console.log('文件关闭了')
 })
+
+// end之后不能写入
+ws.end('456')
 
