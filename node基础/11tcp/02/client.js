060570cb75d986892c97d95d27d11af5251cbd67
maoxiaoxing <930649054@qq.com>
2021-10-09 08:21:44 +0800

    粘包解决方案-定时器

--- node基础/11tcp/02/client.js
+++ node基础/11tcp/02/client.js
@@ -5,11 +5,22 @@ const client = net.createConnection({
   host: '127.0.0.1',
 })
 
+const dataArr = [
+  '小熊饼干1',
+  '小熊饼干2',
+  '小熊饼干3',
+  '小熊饼干4',
+]
+
 client.on('connect', () => {
   client.write('小熊饼干')
-  client.write('小熊饼干1')
-  client.write('小熊饼干2')
-  client.write('小熊饼干3')
+  for(let i = 0; i < dataArr.length; i++) {
+    (function(val, index) {
+      setTimeout(() => {
+        client.write(val)
+      }, 1000 * (index+1))
+    })(dataArr[i], i)
+  }
 })
 
 client.on('data', (chunk) => {
