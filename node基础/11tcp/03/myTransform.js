750871fc217d09d485c5c782dd7949224e8de8d5
maoxiaoxing <930649054@qq.com>
2021-10-10 10:36:14 +0800

    编码

--- node基础/11tcp/03/myTransform.js
+++ node基础/11tcp/03/myTransform.js
@@ -4,4 +4,19 @@ class MyTransfromCode {
     this.serialNum = 0
     this.serialLen = 2
   }
+
+  // 编码
+  encode(data, serialNum) {
+    const body = Buffer.from(data)
+
+    const headerBuf = Buffer.alloc(this.packageHeaderLen)
+    headerBuf.writeInt16BE(serialNum || this.serialNum)
+    headerBuf.writeInt16BE(body.length, this.serialLen)
+
+    if (serialNum == undefined) {
+      serialNum++
+    }
+
+    return Buffer.concat([headerBuf, body])
+  }
 }
