c0306e5d082cc9606cf6b2a2a385cdaa47b4bda1
maoxiaoxing <930649054@qq.com>
2021-10-14 21:44:10 +0800

    容错处理

--- node基础/static-server/server.js
+++ node基础/static-server/server.js
@@ -1,13 +1,26 @@
 const http = require('http')
 const url = require('url')
+const path = require('path')
+const fs = require('fs')
 
 const server = http.createServer((req, res) => {
-  const {
+  let {
     pathname,
     query,
   } = url.parse(req.url)
+  pathname = decodeURIComponent(pathname)
+  const absPath = path.join(__dirname, pathname)
+  console.log(absPath)
 
-  res.end('123')
+  fs.stat(absPath, (err, statObj) => {
+    if(err) {
+      res.statusCode = 404
+      res.end('Not Found')
+      return
+    }
+  })
+
+  // res.end('123')
 })
 
 server.listen(3000, () => {
