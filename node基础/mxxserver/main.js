9644e0bb8373c615f900fa01c3c9a477f71158d7
maoxiaoxing <930649054@qq.com>
2021-10-16 09:14:28 +0800

    服务启动了

--- node基础/mxxserver/main.js
+++ node基础/mxxserver/main.js
@@ -1,12 +1,27 @@
 const http = require('http')
 
+function mergeConfig(config) {
+  return {
+    port: 3000,
+    directory: process.cwd(),
+    ...config
+  }
+}
 class Server {
   constructor(config) {
-    this.config = config
+    this.config = mergeConfig(config)
+    console.log(this.config)
   }
 
   start() {
-    console.log('服务已经运行了')
+    const server = http.createServer(this.serverHandle.bind(this))
+    server.listen(this.config.port, () => {
+      console.log('服务已经启动了...')
+    })
+  }
+
+  serverHandle(req, res) {
+    
   }
 }
 
