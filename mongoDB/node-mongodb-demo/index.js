72da205a4a101c1d0dc0db311b91d8f284229c91
maoxiaoxing <930649054@qq.com>
2021-11-17 08:20:47 +0800

    注释

--- mongoDB/node-mongodb-demo/index.js
+++ mongoDB/node-mongodb-demo/index.js
@@ -7,8 +7,11 @@ async function run () {
     await client.connect()
     const testDb = client.db('test')
     const inventtoryCollection = testDb.collection('inventory')
+    // 插入
     // inventtoryCollection.insertOne({"name": 'jack', "age": 18})
     // inventtoryCollection.insertMany([{"name": 'king', "age": 21}, {"name": 'ben', "age": 19}])
+
+    // 查找
     // const ret = await inventtoryCollection.find({
     //   name: 'ben'
     // })
@@ -17,10 +20,16 @@ async function run () {
     //     $lt: 19
     //   }
     // })
-    const res = await inventtoryCollection.deleteOne({
-      _id: ObjectId('6193b55ff8783c9a5f4262a3')
-    })
-    console.log(res)
+
+    // 删除
+    // const res = await inventtoryCollection.deleteOne({
+    //   _id: ObjectId('6193b55ff8783c9a5f4262a3')
+    // })
+    // console.log(res)
+
+    // 更新
+    
+
     const ret = await inventtoryCollection.find()
     const arrRet = await ret.toArray()
     console.log(arrRet)
