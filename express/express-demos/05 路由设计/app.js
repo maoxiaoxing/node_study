63136daa6b356e329e7ba7f3eab8ce375ea9f862
maoxiaoxing <930649054@qq.com>
2021-11-22 08:20:59 +0800

    获取列表

--- express/express-demos/05 路由设计/app.js
+++ express/express-demos/05 路由设计/app.js
@@ -1,9 +1,26 @@
 const express = require('express')
+const fs = require('fs')
 
 const app = express()
 
 app.get('/todos', (req, res) => {
-  res.send('get todos')
+  fs.readFile('./db.json', 'utf-8', (err, data) => {
+    if (err) {
+      return res.status(500).json({
+        error: err.message
+      })
+    }
+
+    const db = JSON.parse(data)
+    res.status(200).json({
+      errno: 200,
+      errmsg: 'ok',
+      data: {
+        data: db.todos,
+        total: db.todos.length,
+      }
+    })
+  })
 })
 
 app.get('/todos/:id', (req, res) => {
