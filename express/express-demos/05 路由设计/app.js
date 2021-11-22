d1ab1b7c880212123b9e218afb7e5a8d8b37717e
maoxiaoxing <930649054@qq.com>
2021-11-22 08:37:13 +0800

    根据id获取单个任务

--- express/express-demos/05 路由设计/app.js
+++ express/express-demos/05 路由设计/app.js
@@ -3,6 +3,28 @@ const fs = require('fs')
 
 const app = express()
 
+const statusMap = {
+  ok: 200,
+  serviceErr: 500,
+}
+
+const msgMap = new Map([
+  [200, '成功'],
+  [500, '服务端错误'],
+  [404, '没有找到资源'],
+])
+
+const mapStatus = (code, data, restObj) => {
+  return {
+    errno: code,
+    errmsg: msgMap.get(code),
+    data: {
+      data,
+      ...restObj
+    }
+  }
+}
+
 app.get('/todos', (req, res) => {
   fs.readFile('./db.json', 'utf-8', (err, data) => {
     if (err) {
@@ -12,19 +34,28 @@ app.get('/todos', (req, res) => {
     }
 
     const db = JSON.parse(data)
-    res.status(200).json({
-      errno: 200,
-      errmsg: 'ok',
-      data: {
-        data: db.todos,
-        total: db.todos.length,
-      }
-    })
+    res.status(200).json(mapStatus(200, db.todos, {length: db.todos.length}))
   })
 })
 
 app.get('/todos/:id', (req, res) => {
-  res.send('get todos/id')
+  fs.readFile('./db.json', 'utf-8', (err, data) => {
+    if (err) {
+      return res.status(500).json({
+        error: err.message
+      })
+    }
+
+    const db = JSON.parse(data)
+    const todo = db.todos.find((item) => item.id === req.params.id)
+
+    if (!todo) {
+      return res.status(404).json(mapStatus(404))
+    }
+
+    res.status(200).json(mapStatus(200, todo))
+  })
+
 })
 
 app.post('/todos', (req, res) => {
