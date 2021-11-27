caf45b38c47f79c0ea330762674c633c5f3ee982
maoxiaoxing <930649054@qq.com>
2021-11-27 15:39:28 +0800

    配置常用中间件

--- express/realworld/app.js
+++ express/realworld/app.js
@@ -1,6 +1,13 @@
 const express = require('express')
+const morgan = require('morgan')
+const cors = require('cors')
 
 const app = express()
+
+app.use(morgan('dev'))
+app.use(express.json())
+app.use(express.urlencoded())
+app.use(cors())
 
 const PORT = process.env.PORT || 3000
 
