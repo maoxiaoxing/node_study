77e9d52241b028a7a6280a403a36fba47edc5caa
maoxiaoxing <930649054@qq.com>
2021-11-28 10:08:08 +0800

    数据模型

--- express/realworld/app.js
+++ express/realworld/app.js
@@ -2,6 +2,8 @@ const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
 const router = require('./router')
+const errorHandler = require('./middleware/error-handler')
+require('./model')
 
 const app = express()
 
@@ -13,6 +15,9 @@ app.use(cors())
 const PORT = process.env.PORT || 3000
 
 app.use('/api', router)
+
+// 挂载统一处理服务端错误中间件
+app.use(errorHandler())
 
 app.listen(PORT, () => {
   console.log(`server is running at http://localhost:${PORT}`)
