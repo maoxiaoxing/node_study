8009206c4f77db6e3cbe35529b4a99bc0457ebc3
maoxiaoxing <930649054@qq.com>
2021-12-02 08:30:35 +0800

    profile

--- express/realworld/router/profile.js
+++ express/realworld/router/profile.js
@@ -1,21 +1,15 @@
 const express = require('express')
-// const userCtrl = require('../controller/user')
+const userCtrl = require('../controller/user')
 
 const router = express.Router()
 
 // 获取用户资料
-router.get('/:username', (req, res) => {
-  res.send('post /users/login')
-})
+router.get('/:username', userCtrl.getUserProfile)
 
 // 关注用户
-router.post('/:username/follow', (req, res) => {
-  res.send('post /users/login')
-})
+router.post('/:username/follow', userCtrl.followUser)
 
 // 取消关注用户
-router.delete('/:username/follow', (req, res) => {
-  res.send('post /users/login')
-})
+router.delete('/:username/follow', userCtrl.unfollowUser)
 
 module.exports = router
