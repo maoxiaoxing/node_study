2777ea9f9ad06c9e974b19f3ee1e035b977dd590
maoxiaoxing <930649054@qq.com>
2021-12-05 19:53:57 +0800

    抽取app模块

--- express/my-express/express/lib/express.js
+++ express/my-express/express/lib/express.js
@@ -1,32 +1,8 @@
-const http = require('http')
-const url = require('url')
-
-const routes = [
-
-]
+const App = require('./application')
 
 function createApplication () {
-  return {
-    get (path, handler) {
-      routes.push({
-        path,
-        method: 'get',
-        handler,
-      })
-    },
-    listen (...args) {
-      const server = http.createServer((req, res) => {
-        const { pathname } = url.parse(req.url)
-        const method = req.method.toLowerCase()
-        const route = routes.find((route) => route.path === pathname && route.method === method)
-        if (route) {
-          return route.handler(req, res)
-        }
-        res.end('404 Not Found.')        
-      })
-      server.listen(...args)
-    },
-  }
+  const app = new App()
+  return app
 }
 
 module.exports = createApplication
