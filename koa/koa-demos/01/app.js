ae894081a2e2c01fc104493d12dcc4825adbee27
maoxiaoxing <930649054@qq.com>
2021-12-07 08:22:01 +0800

    简单的路由

--- koa/koa-demos/01/app.js
+++ koa/koa-demos/01/app.js
@@ -5,7 +5,18 @@ const app = new Koa()
 // koa 没有路由系统
 
 app.use(ctx => {
-  ctx.body = 'hello koa'
+  console.log(ctx.req.method)
+  console.log(ctx.req.url)
+
+  // ctx.body = 'hello koa'
+  const path = ctx.path
+  if (path === '/') {
+    ctx.body = 'home page'
+  } else if (path === '/foo') {
+    ctx.body = 'foo page'
+  } else {
+    ctx.body = '404 Not Found'
+  }
 })
 
 app.listen(3000, () => {
