4deb8466d3cb784e0b1c63a5fb1eb076a3d41e6a
maoxiaoxing <930649054@qq.com>
2021-12-07 08:28:09 +0800

    koa中的路由

--- koa/koa-demos/01/app.js
+++ koa/koa-demos/01/app.js
@@ -1,23 +1,47 @@
 const Koa = require('koa')
+const Router = require('@koa/router')
 
 const app = new Koa()
 
+const router = new Router()
+
+router.get('/', (ctx) => {
+  ctx.body = 'home page'
+})
+
+router.post('/', (ctx) => {
+  ctx.body = 'post /'
+})
+
+router.get('/foo', (ctx) => {
+  ctx.body = 'get foo'
+})
+
+router.get('/user/:id', (ctx) => {
+  console.log(ctx.params.id)
+  ctx.body = 'get /user/:id'
+})
+
+app
+  .use(router.routes())
+  .use(router.allowedMethods())
+
 // koa 没有路由系统
 
-app.use(ctx => {
-  console.log(ctx.req.method)
-  console.log(ctx.req.url)
+// app.use(ctx => {
+//   console.log(ctx.req.method)
+//   console.log(ctx.req.url)
 
-  // ctx.body = 'hello koa'
-  const path = ctx.path
-  if (path === '/') {
-    ctx.body = 'home page'
-  } else if (path === '/foo') {
-    ctx.body = 'foo page'
-  } else {
-    ctx.body = '404 Not Found'
-  }
-})
+//   // ctx.body = 'hello koa'
+//   const path = ctx.path
+//   if (path === '/') {
+//     ctx.body = 'home page'
+//   } else if (path === '/foo') {
+//     ctx.body = 'foo page'
+//   } else {
+//     ctx.body = '404 Not Found'
+//   }
+// })
 
 app.listen(3000, () => {
   console.log('server is running at http://localhost:3000')
