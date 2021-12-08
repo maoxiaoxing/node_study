aeb3f69aba3792f11fe94d2f7b3e72b04af0c6b0
maoxiaoxing <930649054@qq.com>
2021-12-08 08:25:40 +0800

    异步中间件

--- koa/koa-demos/01/app.js
+++ koa/koa-demos/01/app.js
@@ -3,33 +3,46 @@ const Router = require('@koa/router')
 const static = require('koa-static')
 const path = require('path')
 const mount = require('koa-mount')
+const fs = require('fs')
+const util = require('util')
 
 const app = new Koa()
 
 app.use(mount('/public', static(path.join(__dirname, './public'))))
 
 // 洋葱模型 中间件执行顺序
-const one = (ctx, next) => {
-  console.log('in one')
+// const one = (ctx, next) => {
+//   console.log('in one')
+//   next()
+//   console.log('out one')
+// }
+
+// const two = (ctx, next) => {
+//   console.log('in two')
+//   // next()
+//   console.log('out two')
+// }
+
+// const three = (ctx, next) => {
+//   console.log('in three')
+//   next()
+//   console.log('out three')
+// }
+
+// app.use(one)
+// app.use(two)
+// app.use(three)
+
+
+// 异步中间件
+app.use(async (ctx, next) => {
+  const data = await util.promisify(fs.readFile)('./public/index.html', 'utf8')
+  // const data = await util.promisify(fs.readFile)('./public/foo.html')
+  // ctx.type = 'html'
+  ctx.body = data
   next()
-  console.log('out one')
-}
+})
 
-const two = (ctx, next) => {
-  console.log('in two')
-  // next()
-  console.log('out two')
-}
-
-const three = (ctx, next) => {
-  console.log('in three')
-  next()
-  console.log('out three')
-}
-
-app.use(one)
-app.use(two)
-app.use(three)
 
 
 
