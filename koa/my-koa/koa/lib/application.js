d7232a2b89e14505da8d89717ae090f431f244cc
maoxiaoxing <930649054@qq.com>
2021-12-09 08:30:26 +0800

    user

--- koa/my-koa/koa/lib/application.js
+++ koa/my-koa/koa/lib/application.js
@@ -1,11 +1,53 @@
 const http = require('http')
 
 class Application {
+  constructor () {
+    this.middleware = []
+  }
+
   listen (...args) {
     const server = http.createServer((req, res) => {
-      res.end('my koa')
+      this.callback()
     })
     server.listen(...args)
+  }
+
+  use (fn) {
+    this.middleware.push(fn)
+  }
+
+  compose (middleware) {
+    return function () {
+      const dispatch = (index) => {
+        if (index >= middleware.length) {
+          return Promise.resolve()
+        }
+        const fn = middleware[index]
+        return Promise.resolve(
+          fn({}, () => dispatch(index + 1))
+        )
+      }
+
+      return dispatch(0)
+    }
+  }
+
+  callback () {
+    const fnMiddleware = this.compose(this.middleware)
+    const handleRequest = (req, res) => {
+      fnMiddleware()
+        .then(() => {
+          console.log('end')
+          res.end('cb koa')
+        })
+        .catch((err) => {
+          // console.log('err', err)
+          res.end(err.message)
+        })
+
+    }
+
+    return handleRequest
   }
 }
 
