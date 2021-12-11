52ecc34ab4fe8a0e66ba8813bde03dd9fe837db9
maoxiaoxing <930649054@qq.com>
2021-12-11 09:30:35 +0800

    ctx上添加body

--- koa/my-koa/koa/lib/context.js
+++ koa/my-koa/koa/lib/context.js
@@ -2,10 +2,21 @@ const context = {}
 
 defineProperty('request', 'method')
 defineProperty('request', 'url')
+defineProperty('response', 'body')
+
 
 function defineProperty (target, name) {
-  context.__defineGetter__(name, function () {
-    return this[target][name]
+  // context.__defineGetter__(name, function () {
+  //   return this[target][name]
+  // })
+  Object.defineProperty(context, name, {
+    get () {
+      return this[target][name]
+    },
+
+    set (value) {
+      this[target][name] = value
+    }
   })
   // Object.defineProperty(context, name, {
   //   get () {
--- koa/my-koa/koa/lib/response.js
+++ koa/my-koa/koa/lib/response.js
@@ -1,6 +1,16 @@
 const response = {
   set status (value) {
     this.res.statusCode = value
+  },
+
+  _body: '', // 真正用来存数据的
+
+  get body () {
+    return this._body
+  },
+
+  set body (value) {
+    this._body = value
   }
 }
 
